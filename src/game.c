6cfc1c495f330ffcd9c65bedee6d92e1d46cc267
Kutuzov Dmitry <archie-woc@ya.ru>
2022-01-03 01:26:19 +0300

    Added declaration file and formated snake.c

--- src/game.c
+++ src/game.c
@@ -1,3 +1,30 @@
-//
-// Created by archi on 03.01.2022.
-//
+#include "game.h"
+#include <stdbool.h>
+
+const GameState default_state = {  };
+
+GameState setup_game()
+{
+
+}
+
+bool is_game_over(GameState* game)
+{
+}
+
+void draw_field(GameState* game)
+{
+}
+
+void scan_user_key(GameState* game)
+{
+}
+
+/* Run after void scan_user_key(GameState* game) */
+void next_step(GameState* game)
+{
+}
+
+void free_game(GameState* game)
+{
+}
--- src/snake.c
+++ src/snake.c
@@ -3,12 +3,14 @@
 int main(int argc, char* argv[])
 {
     GameState game = setup_game();
+
     while (!is_game_over(&game)) {
         draw_field(&game);
         scan_user_key(&game);
         next_step(&game);
     }
-	free_game(&game);
 
-	return 0;
+    free_game(&game);
+
+    return 0;
 }
