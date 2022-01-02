1fe489155e638aede6f25aacaa6a46660dbfd271
Dmitry Kutuzov <archie-woc@ya.ru>
2022-01-02 20:33:18 +0300

    Added new branch and new file :)

---
+++ src/aplusb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a, b;
+   
+    printf("Enter two numbers:\n");
+
+    scanf("%d%d", &a, &b);
+    
+    printf("Sum: %d\n", a + b);
+
+    return 0;
+}
