4cfb7c541ff479e43cb708d9ac0aa380d232a697
Kutuzov Dmitry <archie-woc@ya.ru>
2022-01-03 01:47:42 +0300

    GameState definition

--- src/game.h
+++ src/game.h
@@ -1,6 +1,25 @@
 #include <stdbool.h>
 
+typedef enum {
+    NONE = 0,
+    APPLE,
+    SNAKE_TAIL,
+    SNAKE_HEAD
+} SpaceState;
+
+// In c-array x - is a number of column, y - is a row number
 typedef struct {
+    int x, y;
+} Coordinate;
+
+typedef struct {
+    bool is_game_over;
+    size_t field_height;
+    size_t field_width;
+    Coordinate tail_pos;
+    Coordinate head_pos;
+    Coordinate direction;
+    SpaceState** field;
 } GameState;
 
 GameState setup_game();
