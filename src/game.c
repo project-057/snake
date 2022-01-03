c6083832a4e9c7c76710de2ca7bc46d80490de96
Kutuzov Dmitry <archie-woc@ya.ru>
2022-01-03 03:21:51 +0300

    Snake definition and doubly linked list

--- src/game.c
+++ src/game.c
@@ -1,25 +1,29 @@
 #include "game.h"
+#include "utils.h"
 #include <stdbool.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-
-
-int get_random()
-{
-    
-}
 
 GameState setup_game()
 {
     GameState game;
 
+    //  getting field sizes from console
     printf("Enter field sizes:\nHeight: ");
-    scanf("%d", &game.field_height);
+    scanf("%llu", &game.field_height);
     printf(" Width: ");
-    scanf("%d", &game.field_width);
+    scanf("%llu", &game.field_width);
 
+    // Creating snake at random position
+    game.snake.snake_points->next = NULL;
+    game.snake.snake_points->prev = NULL;
+    game.snake.snake_len = 0;
 
+    game.head_pos = get_random_coords(&game);
+    push_back_point(&game.snake, &game.snake.head_pos);
+
+    game.snake.direction.x = rand() % 2;
+    game.snake.direction.y = (x == 0 ? 1 : 0);
 }
 
 bool is_game_over(GameState* game)
@@ -40,5 +44,9 @@ void next_step(GameState* game)
 }
 
 void free_game(GameState* game)
+{
+}
+
+void push_back_point(Snake* snake, const Coordinate* point)
 {
 }
--- src/game.h
+++ src/game.h
@@ -1,3 +1,4 @@
+#include "utils.h"
 #include <stdbool.h>
 
 typedef enum {
@@ -7,14 +8,27 @@ typedef enum {
     SNAKE_HEAD
 } SpaceState;
 
+// Doubly linked list
+typedef struct {
+    Coordinate pos;
+    Point* next;
+    Point* prev;
+} Point;
+
+typedef struct {
+    Coordinate head_pos;
+    //  Direction vector {x, y}, where x and y 1 or 0;
+    Coordinate direction;
+    Point* snake_points;
+    size_t snake_len;
+} Snake;
+
 typedef struct {
     bool is_game_over;
     size_t field_height;
     size_t field_width;
-    Coordinate tail_pos;
-    Coordinate head_pos;
-    Coordinate direction;
     SpaceState** field;
+    Snake snake;
 } GameState;
 
 GameState setup_game();
@@ -24,6 +38,12 @@ bool is_game_over(GameState* game);
 void draw_field(GameState* game);
 
 void scan_user_key(GameState* game);
+
+void push_back_point(Snake* snake, const Coordinate* point);
+
+void push_front_point(Snake* snake, const Coordinate* point);
+
+void remove_back_point(Snake* snake);
 
 /* Run after void scan_user_key(GameState* game) */
 void next_step(GameState* game);
