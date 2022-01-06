a1d3429519a7715dfcd037907af90c9477e564d4
khadievedem <khadiev.edem@gmail.com>
2022-01-06 11:00:29 +0300

    is_game_over function

--- src/utils.c
+++ src/utils.c
@@ -4,18 +4,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-void init_rand() {
+void init_rand()
+{
     srand(time(NULL));
 }
 
-Coordinate get_random_coords(int width, int height) {
+Coordinate get_random_coords(int width, int height)
+{
     Coordinate out = {
-            .x = get_random_integer(0, width),
-            .y = get_random_integer(0, height)
+        .x = get_random_integer(0, width),
+        .y = get_random_integer(0, height)
     };
     return out;
 }
 
-int get_random_integer(int from, int to) {
+int get_random_integer(int from, int to)
+{
     return from + rand() % (to - from);
 }
