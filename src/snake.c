f9194fdd028439d2912f21bc525b20316c41331d
khadievedem <khadiev.edem@gmail.com>
2022-01-12 18:19:56 +0300

    Changed time to sleep

--- src/snake.c
+++ src/snake.c
@@ -12,9 +12,10 @@ int main(int argc, char* argv[])
 
     draw_field(&game);
     while (!is_game_over(&game)) {
-        sleep(1);
+        /* sleep(1); */
         scan_user_key(&game);
         next_step(&game);
+		game.snake.snake_len > 5 ? msleep(130) : msleep(230);
         draw_field(&game);
         /* usleep((unsigned)300000); */
     }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,6 @@
 #include "utils.h"
 
 #include <stdlib.h>
-#include <time.h>
 
 void init_rand()
 {
@@ -20,4 +19,12 @@ Coordinate get_random_coords(int width, int height)
 int get_random_integer(int from, int to)
 {
     return from + rand() % (to - from);
+}
+
+void msleep(int tms)
+{
+    struct timeval tv;
+    tv.tv_sec  = tms / 1000;
+    tv.tv_usec = (tms % 1000) * 1000;
+    select (0, NULL, NULL, NULL, &tv);
 }
