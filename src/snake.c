24c6b253059cf617d7f1b935a633d07d6fea6553
khadievedem <khadiev.edem@gmail.com>
2022-01-12 18:26:39 +0300

    Made format

--- src/snake.c
+++ src/snake.c
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
         /* sleep(1); */
         scan_user_key(&game);
         next_step(&game);
-		game.snake.snake_len > 5 ? msleep(130) : msleep(230);
+        game.snake.snake_len > 5 ? msleep(130) : msleep(230);
         draw_field(&game);
         /* usleep((unsigned)300000); */
     }
--- src/utils.c
+++ src/utils.c
@@ -24,7 +24,7 @@ int get_random_integer(int from, int to)
 void msleep(int tms)
 {
     struct timeval tv;
-    tv.tv_sec  = tms / 1000;
+    tv.tv_sec = tms / 1000;
     tv.tv_usec = (tms % 1000) * 1000;
-    select (0, NULL, NULL, NULL, &tv);
+    select(0, NULL, NULL, NULL, &tv);
 }
